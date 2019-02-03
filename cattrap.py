fbf5aa21c04833fd60d0e327a8b3b611b15c6114
barlaensdoonn <brandon.aleson@gmail.com>
2019-02-02 19:37:21 -0800

    can't trap all day, gotta rest

--- cattrap.py
+++ cattrap.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 
 from time import sleep
+from datetime import datetime
 from gpiozero import DistanceSensor
 
 sensor = DistanceSensor(echo=4, trigger=5)
 
-while True:
-        print('Distance: ', sensor.distance * 100)
+
+def check_curfew(start=21, stop=9):
+    now = datetime.now()
+    if now.hour >= start or now.hour <= stop:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    while True:
+        run = check_curfew()
+
+        while run:
+            print('Distance: ', sensor.distance * 100)
         
-        if sensor.distance <= 0.5:
-            print('i see you')
-            # spray the cat
+            if sensor.distance <= 0.5:
+                print('i see you')
+                # spray the cat
         
-        sleep(0.25)
+            sleep(0.25)
+            run = check_curfew()
 
